59b34cc2a9e8f92446fb7336cfa26aa511766a04
jljl1337 <lckjack123@gmail.com>
2025-09-25 16:40:31 -0600

    refactor: restructure layout of books page for improved styling and responsiveness

--- web/app/routes/books/index.tsx
+++ web/app/routes/books/index.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Book</h1>
+    <div className="h-full flex items-center justify-center">
+      <div className="h-full max-w-[90rem] flex-1 flex flex-col p-8 gap-4">
+        <h1 className="text-2xl font-bold mb-4">Books</h1>
+      </div>
     </div>
   );
 }
