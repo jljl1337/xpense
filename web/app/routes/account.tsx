8e4e588833e4d8be203d2152bcf4f3efa769fc93
jljl1337 <lckjack123@gmail.com>
2025-10-01 23:10:27 -0600

    feat: increase header size for Account and Books pages for improved visibility

--- web/app/routes/account.tsx
+++ web/app/routes/account.tsx
@@ -118,7 +118,7 @@ export default function Page({ loaderData }: Route.ComponentProps) {
       <title>Account | Xpense</title>
       <div className="h-full flex items-center justify-center">
         <div className="h-full max-w-[90rem] flex-1 flex flex-col p-8 gap-4">
-          <h1 className="text-2xl font-bold mb-4">Account</h1>
+          <h1 className="text-4xl">Account</h1>
           {loaderData?.data ? (
             <div>
               <p className="mb-2">User ID: {loaderData.data.user.id}</p>
--- web/app/routes/books/index.tsx
+++ web/app/routes/books/index.tsx
@@ -102,7 +102,7 @@ export default function Page({ loaderData }: Route.ComponentProps) {
       <title>Books | Xpense</title>
       <div className="h-full flex items-center justify-center">
         <div className="h-full max-w-[90rem] flex-1 flex flex-col p-8 gap-4">
-          <h1 className="text-2xl font-bold mb-4">Books</h1>
+          <h1 className="text-4xl">Books</h1>
           <Button className="w-24" asChild>
             <Link to="/books/create">Create</Link>
           </Button>
