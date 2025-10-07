3ed2aab53ee9c33bc14e2e00d3b2a8bbc3d5e3f1
jljl1337 <lckjack123@gmail.com>
2025-10-07 15:55:35 -0600

    feat: redirect authenticated user in root route

--- web/app/routes/home.tsx
+++ web/app/routes/home.tsx
@@ -1,5 +1,13 @@
 import { redirect } from "react-router";
 
+import { getCsrfToken } from "~/lib/db/auth";
+import { isUnauthorizedError } from "~/lib/db/common";
+
 export async function clientLoader() {
-  return redirect("/auth/sign-in");
+  const csrfToken = await getCsrfToken();
+
+  if (csrfToken.error != null && isUnauthorizedError(csrfToken.error)) {
+    return redirect("/auth/sign-in");
+  }
+  return redirect("/books");
 }
