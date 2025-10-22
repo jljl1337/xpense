8b7bb6f9ea58f7f395bde686932ac395a7385223
jljl1337 <lckjack123@gmail.com>
2025-10-22 01:42:55 -0600

    feat: enhance category deletion with expense count check

--- web/app/routes/categories/delete.tsx
+++ web/app/routes/categories/delete.tsx
@@ -5,6 +5,7 @@ import DeletePage from "~/components/pages/delete-page";
 import { getCsrfToken } from "~/lib/db/auth";
 import { deleteCategory, getCategory } from "~/lib/db/categories";
 import { redirectIfNeeded } from "~/lib/db/common";
+import { getExpensesCountByBookID } from "~/lib/db/expenses";
 
 export async function clientLoader({ params }: Route.LoaderArgs) {
   const [csrfToken, category] = await Promise.all([
@@ -14,7 +15,18 @@ export async function clientLoader({ params }: Route.LoaderArgs) {
 
   redirectIfNeeded(csrfToken.error, category.error);
 
-  return { csrfToken: csrfToken.data!, category: category.data! };
+  const expenseCount = await getExpensesCountByBookID(
+    category.data!.bookID,
+    category.data!.id,
+  );
+
+  redirectIfNeeded(expenseCount.error);
+
+  return {
+    csrfToken: csrfToken.data!,
+    category: category.data!,
+    expenseCount: expenseCount.data!,
+  };
 }
 
 export default function Page({ loaderData }: Route.ComponentProps) {
@@ -30,12 +42,18 @@ export default function Page({ loaderData }: Route.ComponentProps) {
     return { error: null };
   }
 
+  let description = `Are you sure you want to delete the following category: ${loaderData.category.name}?`;
+
+  if (loaderData.expenseCount > 0) {
+    description += ` The associated ${loaderData.expenseCount} expense(s) will also be deleted. This action cannot be undone.`;
+  }
+
   return (
     <>
       <title>Delete Category | Xpense</title>
       <DeletePage
         title="Delete Category"
-        description={`Are you sure you want to delete the following category: ${loaderData.category.name}?`}
+        description={description}
         action={action}
         redirectTo={`/books/${loaderData.category.bookID}/categories`}
       />
